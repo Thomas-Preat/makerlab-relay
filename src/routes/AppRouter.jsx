21909785837ff485a1516e08ea0072db207d836f
Thomas-Preat <preatthomas@gmail.com>
2026-02-23 13:18:33 +0100

    Temp fix Login not used

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
+//import Login from "../pages/Login";
 import Inventory from "../pages/Inventory";
 import Dashboard from "../pages/Dashboard";
 import DocLayout from "../components/documentation/DocLayout";
